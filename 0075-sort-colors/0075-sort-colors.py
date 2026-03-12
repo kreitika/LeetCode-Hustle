ba4dc78ca4d6939b50039f3dc8a9c9d30c0c03fd
kreitika <121768189+kreitika@users.noreply.github.com>
2026-03-12 20:13:59 +0530

    Time: 0 ms (100%), Space: 19.4 MB (21.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,23 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def partition(arr, l, r):
-            pivot = arr[r]
-            p = l
-            for i in range(l, r):
-                if arr[i] <= pivot:
-                    arr[p], arr[i] = arr[i], arr[p]
-                    p += 1
-            arr[p], arr[r] = arr[r], arr[p]
-            return p
+        max,min = 0, 0
+        for el in nums:
+            if el >= max: max = el
+            if el <= min: min = el
 
-        def quickSort(arr, l, r):
-            if l >= r:
-                return
-            p = partition(arr, l, r)
-            quickSort(arr, l, p-1)
-            quickSort(arr, p+1, r)
-            
-        quickSort(nums, 0, len(nums)-1)
-        return nums
-    
+        if min < 0: offset = min
+        else: offset = 0
+
+        arr = [0]*(max - min + 1)
+
+        for n in nums:
+            arr[n + offset]+=1
+        print(arr)
+        
+        i,j = 0,0
+        while i < len(arr):
+            while arr[i] > 0:
+                nums[j] = i + offset
+                j+=1
+                arr[i]-=1
+            i+=1
