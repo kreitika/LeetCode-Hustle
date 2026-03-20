1174d1f509400436d48922d2fac7db06aa0f7157
kreitika <121768189+kreitika@users.noreply.github.com>
2026-03-20 14:55:21 +0530

    Time: 17 ms (5.3%), Space: 21.9 MB (92.58%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if len(nums) == 1 : return nums
+        if len(nums) == 2 :
+            if nums[0] != nums[1] : return nums
+            else : 
+                arr = [nums[0]]
+                return arr
+
+        cand1 = nums[0]
+        cand2 = 0
+        for i in range(len(nums)):
+            if nums[i] != cand1 :
+                 cand2 = nums[i]
+                 break
+
+        count1, count2 = 0, 0
+        for i in range( len(nums)):
+            if nums[i] == cand1 : count1+=1
+            elif nums[i] == cand2 : count2+=1
+            
+            elif count1 == 0 : 
+                cand1 = nums[i]
+                count1 = 1
+            elif count2 == 0 : 
+                cand2 = nums[i]
+                count2 = 1
+            else : 
+                count1-=1
+                count2-=1
+        print(cand1, cand2)
+        temp1, temp2 = 0, 0
+        for i in range(len(nums)):
+            if nums[i] == cand1 : temp1+=1
+            if nums[i] == cand2 and cand2 != cand1 : temp2+=1
+        ans = []
+        if temp1 > len(nums)//3 : ans.append(cand1)
+        if temp2 > len(nums)//3 : ans.append(cand2)
+
+        return ans
