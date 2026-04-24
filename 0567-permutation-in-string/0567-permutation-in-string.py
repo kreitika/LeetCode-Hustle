67442fc74dc1684616868e608a58085d213c81ca
kreitika <121768189+kreitika@users.noreply.github.com>
2026-04-25 05:29:28 +0530

    Time: 19 ms (61.95%), Space: 19.6 MB (22.16%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2) : return False 
-        count1 = [0]*26
-        count2 = [0]*26
-        for i in s1:
-            count1[ord(i) - ord('a')]+=1
-        
+        if len(s1) > len(s2):
+            return False
+
+        count1 = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
+        count2 = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
+
+        # build s1 and first window of s2
+        for i in range(len(s1)):
+            count1[s1[i]] += 1
+            count2[s2[i]] += 1
+
+        # count matches
+        matches = 0
+        for c in count1:
+            if count1[c] == count2[c]:
+                matches += 1
+
         L = 0
-        for R in range(len(s2)) :
-            count2[ord(s2[R]) - ord('a')] += 1
-            if R - L + 1 > len(s1):
-                count2[ord(s2[L]) - ord('a')] -= 1
-                L += 1
-            if R - L + 1 == len(s1):
-                if count1 == count2: return True
-        return False
+        for R in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
 
+        # add new char
+            char = s2[R]
+            count2[char] += 1
+            if count2[char] == count1[char]:
+                matches += 1
+            elif count2[char] == count1[char] + 1:
+                matches -= 1
 
-        
+        # remove left char
+            char = s2[L]
+            count2[char] -= 1
+            if count2[char] == count1[char]:
+                matches += 1
+            elif count2[char] == count1[char] - 1:
+                matches -= 1
+
+            L += 1
+
+        return matches == 26
